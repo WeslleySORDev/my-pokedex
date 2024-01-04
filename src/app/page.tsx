564b28bfac42ecac38bcf81f814bdeb33b43bf0c
WeslleySORDev <weslleysordev@gmail.com>
2024-01-04 20:47:50 -0300

    fix: layout break mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,7 +71,7 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen gap-4 items-center justify-between relative pt-4">
       <div className="max-w-[407px] relative">
-        <div className="flex gap-4 flex-wrap min-h-screen px-4">
+        <div className="flex gap-4 flex-wrap min-h-screen sm:px-4 items-center">
           {!isLoading
             ? data.map((pokemon: IPokemon) => {
                 return (
@@ -88,7 +88,7 @@ export default function Home() {
                 <SkeletonCard key={i} />
               ))}
         </div>
-        <div className="flex gap-8 items-center sticky bottom-0 bg-white w-full justify-between p-2">
+        <div className="flex sm:gap-8 items-center sticky bottom-0 bg-white w-full justify-between p-2">
           <button
             className="p-4 rounded-md bg-cyan-300 text-zinc-500"
             onClick={() => handlePage(page - 1)}
@@ -96,29 +96,31 @@ export default function Home() {
           >
             Anterior
           </button>
-          <input
-            value={pageFront}
-            onChange={(e) => {
-              if (
-                parseInt(e.currentTarget.value) >
-                Math.ceil(1302 / MAX_ITEMS_ON_PAGE - 1)
-              ) {
-                return setPageFront(Math.ceil(1302 / MAX_ITEMS_ON_PAGE));
-              }
-              if (parseInt(e.currentTarget.value) <= 0) {
-                return setPageFront(1);
-              }
-              return setPageFront(parseInt(e.currentTarget.value));
-            }}
-            onKeyDown={handleKeyDown}
-            className={`p-4 text-center ${
-              pageFront - 1 !== page ? "bg-red-500" : "bg-red-300"
-            }`}
-            type="number"
-            min={1}
-            max={Math.ceil(1302 / MAX_ITEMS_ON_PAGE)}
-          />
-          {/* <span className="p-4 bg-red-300">{page + 1} / {Math.ceil(1302/MAX_ITEMS_ON_PAGE)}</span> */}
+          <div className="flex items-center gap-4">
+            <input
+              value={pageFront}
+              onChange={(e) => {
+                if (
+                  parseInt(e.currentTarget.value) >
+                  Math.ceil(1302 / MAX_ITEMS_ON_PAGE - 1)
+                ) {
+                  return setPageFront(Math.ceil(1302 / MAX_ITEMS_ON_PAGE));
+                }
+                if (parseInt(e.currentTarget.value) <= 0) {
+                  return setPageFront(1);
+                }
+                return setPageFront(parseInt(e.currentTarget.value));
+              }}
+              onKeyDown={handleKeyDown}
+              className={`p-4 text-center ${
+                pageFront - 1 !== page ? "bg-red-500" : "bg-red-300"
+              }`}
+              type="number"
+              min={1}
+              max={Math.ceil(1302 / MAX_ITEMS_ON_PAGE)}
+            />
+            <span className="hidden sm:block">de {Math.ceil(1302 / MAX_ITEMS_ON_PAGE)}</span>
+          </div>
           <button
             className="p-4 rounded-md bg-cyan-300 text-zinc-500"
             onClick={() => handlePage(page + 1)}
