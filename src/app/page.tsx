3f20ef50133af7cac3b0abdfd78891056f64b800
WeslleySORDev <weslleysordev@gmail.com>
2024-01-05 19:49:58 -0300

    feat: pokedex logo add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,23 +69,48 @@ export default function Home() {
   }
 
   return (
-    <main className="flex flex-col min-h-screen gap-4 items-center justify-between relative">
-        <div className="flex gap-2 flex-wrap px-3 py-6 max-w-[360px]">
-          {!isLoading
-            ? data.map((pokemon: IPokemon) => {
-                return (
-                  <PokemonCard
-                    name={pokemon.name}
-                    id={pokemon.id.toString()}
-                    key={pokemon.name + pokemon.id}
-                  />
-                );
-              })
-            : Array.from({ length: MAX_ITEMS_ON_PAGE }, (_, i) => (
-                <SkeletonCard key={i} />
-              ))}
+    <main className="flex flex-col gap-4 items-center justify-between bg-identity-primary max-w-[360px] mx-auto p-1">
+      <div className="flex flex-col gap-2 px-3 pt-3 pb-6 w-full">
+        <div className="flex gap-4 items-center">
+          <div className="relative w-[24px] h-[24px]">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M14.8572 12C14.8572 13.578 13.578 14.8571 12.0001 14.8571C10.4221 14.8571 9.14292 13.578 9.14292 12C9.14292 10.422 10.4221 9.14286 12.0001 9.14286C13.578 9.14286 14.8572 10.422 14.8572 12Z"
+                fill="white"
+              />
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M12.0001 24C18.0454 24 23.0467 19.5296 23.8785 13.7143H16.8503C16.1443 15.7118 14.2393 17.1429 12.0001 17.1429C9.76083 17.1429 7.85584 15.7118 7.14984 13.7143H0.121582C0.953404 19.5296 5.95468 24 12.0001 24ZM7.14984 10.2857H0.121582C0.953404 4.47035 5.95468 0 12.0001 0C18.0454 0 23.0467 4.47035 23.8785 10.2857H16.8503C16.1443 8.28824 14.2393 6.85714 12.0001 6.85714C9.76083 6.85714 7.85584 8.28824 7.14984 10.2857ZM14.8572 12C14.8572 13.578 13.578 14.8571 12.0001 14.8571C10.4221 14.8571 9.14292 13.578 9.14292 12C9.14292 10.422 10.4221 9.14286 12.0001 9.14286C13.578 9.14286 14.8572 10.422 14.8572 12Z"
+                fill="white"
+              />
+            </svg>
+          </div>
+          <h1 className="headline text-grayscale-white">Pokédex</h1>
         </div>
-        {/* <div className="flex items-center sticky bottom-0 bg-white w-full justify-between p-2">
+      </div>
+      <div className="flex justify-center gap-2 flex-wrap px-3 py-6 bg-grayscale-white rounded-lg shadow-inner2dp">
+        {!isLoading
+          ? data.map((pokemon: IPokemon) => {
+              return (
+                <PokemonCard
+                  name={pokemon.name}
+                  id={pokemon.id.toString()}
+                  key={pokemon.name + pokemon.id}
+                />
+              );
+            })
+          : Array.from({ length: MAX_ITEMS_ON_PAGE }, (_, i) => (
+              <SkeletonCard key={i} />
+            ))}
+      </div>
+      {/* <div className="flex items-center sticky bottom-0 bg-white w-full justify-between p-2">
           <button
             className="p-4 rounded-md bg-cyan-300 text-zinc-500"
             onClick={() => handlePage(page - 1)}
