22f8ab5ea27486184267c18e7350859426cb7dfb
WeslleySORDev <weslleysordev@gmail.com>
2024-01-08 15:32:26 -0300

    feat: new pagination system to first and last page

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -15,24 +15,26 @@ export function Pagination({
   const first = Math.min(Math.max(currentPage - MAX_LEFT, 1), maxFirst);
   return (
     <div className="mt-6 flex w-full items-center px-0">
-      <button
-        className={`hidden rounded-sm bg-grayscale-background p-2 disabled:cursor-not-allowed disabled:bg-transparent sm:block`}
-        onClick={() => handleCurrentPage(currentPage - 1)}
-        disabled={currentPage === 1}
-      >
-        Anterior
-      </button>
-      {currentPage > MAX_ITEMS && (
-        <div className="flex items-center gap-2">
-          <button
-            onClick={() => handleCurrentPage(1)}
-            className="text-grayscale-white"
-          >
-            1
-          </button>
-          <span>...</span>
-        </div>
-      )}
+      <div className="flex items-center gap-4">
+        <button
+          className={`hidden rounded-sm bg-grayscale-background p-2 disabled:cursor-not-allowed disabled:bg-transparent sm:block`}
+          onClick={() => handleCurrentPage(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Anterior
+        </button>
+        {currentPage > MAX_LEFT + 1 && (
+          <div className="flex items-center gap-2">
+            <button
+              onClick={() => handleCurrentPage(1)}
+              className="text-grayscale-white"
+            >
+              1
+            </button>
+            <span className="text-grayscale-white">...</span>
+          </div>
+        )}
+      </div>
       <ul className="flex flex-1 items-center justify-center gap-4">
         {Array.from({
           length: Math.min(MAX_ITEMS, Math.ceil(1302 / MAX_ITEMS_ON_PAGE)),
@@ -55,25 +57,26 @@ export function Pagination({
             ) : null,
           )}
       </ul>
-      {currentPage > Math.ceil(1302 / MAX_ITEMS_ON_PAGE) - MAX_ITEMS && (
-        <div className="flex items-center gap-2">
-          <span>...</span>
-
-          <button
-            onClick={() => handleCurrentPage(27)}
-            className="text-grayscale-white"
-          >
-            27
-          </button>
-        </div>
-      )}
-      <button
-        className={`hidden rounded-sm bg-grayscale-background p-2 disabled:cursor-not-allowed disabled:bg-transparent sm:block`}
-        onClick={() => handleCurrentPage(currentPage + 1)}
-        disabled={currentPage === Math.ceil(1302 / MAX_ITEMS_ON_PAGE)}
-      >
-        Próxima
-      </button>
+      <div className="flex items-center gap-4">
+        {currentPage < Math.ceil(1302 / MAX_ITEMS_ON_PAGE) - MAX_LEFT && (
+          <div className="flex items-center gap-2">
+            <span className="text-grayscale-white">...</span>
+            <button
+              onClick={() => handleCurrentPage(27)}
+              className="text-grayscale-white"
+            >
+              27
+            </button>
+          </div>
+        )}
+        <button
+          className={`hidden rounded-sm bg-grayscale-background p-2 disabled:cursor-not-allowed disabled:bg-transparent sm:block`}
+          onClick={() => handleCurrentPage(currentPage + 1)}
+          disabled={currentPage === Math.ceil(1302 / MAX_ITEMS_ON_PAGE)}
+        >
+          Próxima
+        </button>
+      </div>
     </div>
   );
 }
