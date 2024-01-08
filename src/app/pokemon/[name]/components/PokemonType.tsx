bbc89adb6f54c0fbcfc4c9a321ffdad902ba4aa7
WeslleySORDev <weslleysordev@gmail.com>
2024-01-08 09:57:12 -0300

    feat: pokemon image and pokemon type component

---
+++ src/app/pokemon/[name]/components/PokemonType.tsx
@@ -0,0 +1,7 @@
+export function PokemonType() {
+  return (
+    <span className="px-2 py-[2px] rounded-[10px] bg-grayscale-medium text-grayscale-white subtitle-3">
+      Type
+    </span>
+  );
+}
