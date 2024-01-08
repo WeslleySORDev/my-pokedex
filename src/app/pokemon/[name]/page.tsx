7d3714aa8c5b6ec0dcd81282e593f61c4d5306c2
WeslleySORDev <weslleysordev@gmail.com>
2024-01-08 09:47:05 -0300

    feat: poke atribute add base

--- src/app/pokemon/[name]/page.tsx
+++ src/app/pokemon/[name]/page.tsx
@@ -67,7 +67,7 @@ export default function Pokemon({ params }: { params: { name: string } }) {
         </h1>
         <span className="subtitle-2 text-grayscale-white">#999</span>
       </div>
-      <div className="max-h-[640px] min-h-[640px] mt-auto w-full bg-grayscale-white relative flex flex-col rounded-lg shadow-inner2dp">
+      <div className="max-h-[640px] min-h-[640px] mt-auto w-full bg-grayscale-white relative flex flex-col gap-4 rounded-lg shadow-inner2dp pt-14 px-2 sm:px-5 pb-5">
         <div className="absolute -top-36 left-1/2 -translate-x-1/2 z-10">
           <svg
             width="200"
@@ -95,6 +95,69 @@ export default function Pokemon({ params }: { params: { name: string } }) {
               />
             </defs>
           </svg>
+        </div>
+        <div className="flex gap-4 items-center w-full justify-center">
+          <span className="px-2 py-[2px] rounded-[10px] bg-grayscale-medium text-grayscale-white subtitle-3">
+            Type
+          </span>
+          <span className="px-2 py-[2px] rounded-[10px] bg-grayscale-medium text-grayscale-white subtitle-3">
+            Type
+          </span>
+        </div>
+        <span className="text-grayscale-wireframe subtitle-1 text-center">
+          About
+        </span>
+        <div className="flex w-full h-16">
+          <div className="flex flex-col flex-1">
+            <div className="flex fill-grayscale-dark text-grayscale-dark gap-2 justify-center my-auto">
+              <svg
+                width="17"
+                height="16"
+                viewBox="0 0 17 16"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M4.28333 13H13.05L12 5.66663H5.33333L4.28333 13ZM8.66667 4.66663C8.95556 4.66663 9.19444 4.5694 9.38333 4.37496C9.57222 4.18051 9.66667 3.9444 9.66667 3.66663C9.66667 3.37774 9.57222 3.13885 9.38333 2.94996C9.19444 2.76107 8.95556 2.66663 8.66667 2.66663C8.38889 2.66663 8.15278 2.76107 7.95833 2.94996C7.76389 3.13885 7.66667 3.37774 7.66667 3.66663C7.66667 3.9444 7.76389 4.18051 7.95833 4.37496C8.15278 4.5694 8.38889 4.66663 8.66667 4.66663ZM10.4 4.66663H12C12.2556 4.66663 12.4778 4.74718 12.6667 4.90829C12.8556 5.0694 12.9667 5.27774 13 5.53329L14.0333 12.8666C14.0778 13.1666 14.0028 13.4305 13.8083 13.6583C13.6139 13.8861 13.3611 14 13.05 14H4.28333C3.97222 14 3.71945 13.8861 3.525 13.6583C3.33056 13.4305 3.25556 13.1666 3.3 12.8666L4.33333 5.53329C4.36667 5.27774 4.47778 5.0694 4.66667 4.90829C4.85556 4.74718 5.07778 4.66663 5.33333 4.66663H6.93333C6.84444 4.51107 6.77778 4.35274 6.73333 4.19163C6.68889 4.03051 6.66667 3.85551 6.66667 3.66663C6.66667 3.11107 6.86111 2.63885 7.25 2.24996C7.63889 1.86107 8.11111 1.66663 8.66667 1.66663C9.22222 1.66663 9.69444 1.86107 10.0833 2.24996C10.4722 2.63885 10.6667 3.11107 10.6667 3.66663C10.6667 3.85551 10.6444 4.03051 10.6 4.19163C10.5556 4.35274 10.4889 4.51107 10.4 4.66663ZM4.28333 13H13.05H4.28333Z"
+                  fill="#1D1D1D"
+                />
+              </svg>
+              <span className="body-3">9,9 kg</span>
+            </div>
+            <span className="text-center mt-auto caption text-grayscale-medium">
+              Weight
+            </span>
+          </div>
+          <div className="w-[1px] h-full bg-grayscale-light"></div>
+          <div className="flex flex-col flex-1">
+            <div className="flex fill-grayscale-dark text-grayscale-dark gap-2 justify-center my-auto">
+              <svg
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M4 2.33337C4 2.06671 4.1 1.83337 4.3 1.63337C4.5 1.43337 4.73333 1.33337 5 1.33337L11 1.33337C11.2556 1.33337 11.4861 1.43337 11.6917 1.63337C11.8972 1.83337 12 2.06671 12 2.33337V13.6667C12 13.9334 11.8972 14.1667 11.6917 14.3667C11.4861 14.5667 11.2556 14.6667 11 14.6667H5C4.73333 14.6667 4.5 14.5667 4.3 14.3667C4.1 14.1667 4 13.9334 4 13.6667L4 2.33337ZM5 2.33337L5 13.6667H11V11.5H8V10.5H11V8.50004H8V7.50004H11V5.50004H8V4.50004H11V2.33337L5 2.33337ZM8 4.50004V5.50004V4.50004ZM8 7.50004V8.50004V7.50004ZM8 10.5V11.5V10.5Z"
+                  fill="#1D1D1D"
+                />
+              </svg>
+
+              <span className="body-3">9,9 m</span>
+            </div>
+            <span className="text-center mt-auto caption text-grayscale-medium">
+              Height
+            </span>
+          </div>
+          <div className="w-[1px] h-full bg-grayscale-light"></div>
+          <div className="flex flex-col flex-1">
+            <div className="flex flex-col text-grayscale-dark gap-1 justify-center items-center my-auto">
+                <span className="text-grayscale-dark body-3">Ability 1</span>
+                <span className="text-grayscale-dark body-3">Ability 1</span>
+            </div>
+            <span className="text-center mt-auto caption text-grayscale-medium">
+              Moves
+            </span>
+          </div>
         </div>
       </div>
       <div className="absolute top-1 right-1">
