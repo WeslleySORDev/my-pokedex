d7e2260cb25f5e5458e7675b1c3ac4be2e59b9b9
WeslleySORDev <weslleysordev@gmail.com>
2024-01-08 21:59:00 -0300

    fix: last page bug

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -12,9 +12,10 @@ export function Pagination({
   MAX_ITEMS_ON_PAGE,
 }: IPagination) {
   const size = useWindowSize();
+  const pages = Math.ceil(1302 / MAX_ITEMS_ON_PAGE);
   const MAX_ITEMS = size.width <= 375 ? 3 : size.width <= 768 ? 5 : 9;
   const MAX_LEFT = (MAX_ITEMS - 1) / 2;
-  const maxFirst = Math.max(Math.ceil(1302 / MAX_ITEMS_ON_PAGE) - MAX_ITEMS, 1);
+  const maxFirst = Math.max(pages - (MAX_ITEMS - 1), 1);
   const first = Math.min(Math.max(currentPage - MAX_LEFT, 1), maxFirst);
   return (
     <div className="mt-6 flex w-full items-center rounded bg-grayscale-white px-2 xs:px-4">
@@ -48,32 +49,27 @@ export function Pagination({
         </div>
       </div>
       <ul className="flex flex-1 items-center justify-center">
-        {Array.from({
-          length: Math.min(MAX_ITEMS, Math.ceil(1302 / MAX_ITEMS_ON_PAGE)),
-        })
+        {Array.from({ length: Math.min(MAX_ITEMS, pages) })
           .map((_, index) => index + first)
-          .map((page) =>
-            page < Math.ceil(1302 / MAX_ITEMS_ON_PAGE) ? (
-              <li className="text-xs sm:text-sm" key={page}>
-                <button
-                  onClick={() => handleCurrentPage(page)}
-                  className={
-                    page === currentPage
-                      ? "bg-[#7b8795] px-4 py-2 text-grayscale-white"
-                      : "px-4 py-2 text-grayscale-dark"
-                  }
-                >
-                  {page}
-                </button>
-              </li>
-            ) : null,
-          )}
+          .map((page) => (
+            <li className="text-xs sm:text-sm" key={page}>
+              <button
+                onClick={() => handleCurrentPage(page)}
+                className={
+                  page === currentPage
+                    ? "bg-[#7b8795] px-4 py-2 text-grayscale-white"
+                    : "px-4 py-2 text-grayscale-dark"
+                }
+              >
+                {page}
+              </button>
+            </li>
+          ))}
       </ul>
       <div className="flex items-center gap-4 xs:gap-6 sm:gap-8">
         <div
           className={`ml-auto flex items-center gap-2 text-xs sm:text-sm ${
-            currentPage >= Math.ceil(1302 / MAX_ITEMS_ON_PAGE) - MAX_LEFT &&
-            "invisible"
+            currentPage >= pages - MAX_LEFT && "invisible"
           }`}
         >
           <span className="text-grayscale-dark">...</span>
@@ -87,7 +83,7 @@ export function Pagination({
         <button
           className={`rounded-sm fill-[#000000] disabled:cursor-not-allowed`}
           onClick={() => handleCurrentPage(currentPage + 1)}
-          disabled={currentPage === Math.ceil(1302 / MAX_ITEMS_ON_PAGE)}
+          disabled={currentPage === pages}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
